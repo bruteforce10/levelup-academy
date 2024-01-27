bff1cc73177e17f46d09bc1509bea75288d8343e
bruteforce10 <audifirdi@gmail.com>
2024-01-27 13:55:27 +0700

    fix v7

--- pages/api/email.js
+++ pages/api/email.js
@@ -3,33 +3,38 @@ import fs from "fs";
 import Mustache from "mustache";
 
 export default async function handler(req, res) {
-  const { to, subject, data } = req.body;
+  if (req.method === "POST") {
+    const { to, subject, data } = req.body;
 
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    auth: {
-      user: "lv.classonline@gmail.com",
-      pass: "jimx xxbu qccw cabh",
-    },
-  });
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "lv.classonline@gmail.com",
+        pass: "jimx xxbu qccw cabh",
+      },
+    });
 
-  let template = fs.readFileSync("view/otp.html", "utf8");
+    let template = fs.readFileSync("view/otp.html", "utf8");
 
-  let mailOptions = {
-    from: "lv.classonline@gmail.com",
-    to,
-    subject,
-    html: Mustache.render(template, data),
-  };
+    let mailOptions = {
+      from: "lv.classonline@gmail.com",
+      to,
+      subject,
+      html: Mustache.render(template, data),
+    };
 
-  try {
-    const info = await transporter.sendMail(mailOptions);
-    res.status(200).json({ success: true });
-  } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ success: false, error: "Internal Server Error" });
+    try {
+      const info = await transporter.sendMail(mailOptions);
+      res.status(200).json({ success: true });
+    } catch (error) {
+      console.error("Error:", error);
+      res.status(500).json({ success: false, error: "Internal Server Error" });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res.status(405).json({ error: "Method Not Allowed" });
   }
 }
